d4aa3bd15fbcee8c6e17a24be40d0a0f473b5c03
Pedro Costa <pedroh.costa083@gmail.com>
2022-11-19 17:33:57 -0300

    Utilizando dados das APIS - climaAgora_1.5

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,10 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
+const weatherContainer = document.querySelector("#weather-data")
+const backgroundIMG = document.querySelector("#bkIMG")
 
 
 // Funções
@@ -19,13 +21,25 @@ const getWeatherData = async (city) => {
 
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
-    console.log(data);
+    return data
 
 }
 
-const showWeatherData = (city) => {
-    getWeatherData(city);
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+    weatherContainer.classList.remove("hide");
+    backgroundIMG.setAttribute("src",)
 }
+
+
 
 // Eventos
 searchBtn.addEventListener("click", (e) => {
@@ -33,4 +47,12 @@ searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const city = cityInput.value;
     showWeatherData(city);
+})
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
 })
