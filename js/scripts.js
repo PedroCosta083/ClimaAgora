43359baee952c4040595287048beb9bab563b9ea
Pedro Costa <pedroh.costa083@gmail.com>
2022-11-19 15:47:22 -0300

    Recebendo valor do input

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
-//f629271334f82304007c2677e219606f -- API KEY
+const apiKey = "f629271334f82304007c2677e219606f";
+const apiCountryURL = "https://countryflagsapi.com/png/";
 
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// Funções
+const showWeatherData = (city) => {
+    console.log(city);
+}
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+})
